c0c0e62342c363d83d5058a711bb310e3aceb34d
Owlz <whootandahalf@gmail.com>
2016-03-06 16:43:38 -0500

    Adding test case for path

---
+++ tests/test_path.py
@@ -0,0 +1,20 @@
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
+
+import symbolicExecutor
+import ast
+import z3
+from pyPath import Path
+
+test1 = "x = 1\ny = 2"
+
+def test_basicPathStep():
+    b = ast.parse(test1).body
+    p = Path(b,source=test1)
+    p.step()
+    p.step()
+    p.printBacktrace()
+    assert p.state.any_int('x') == 1
+    assert p.state.any_int('y') == 2
+
