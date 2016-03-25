8b08bee2d308bc21b9140dd8dfe0156684458c03
Owlz <whootandahalf@gmail.com>
2016-03-24 20:36:49 -0400

    Refactoring Subscript

--- pyState/Subscript.py
+++ pyState/Subscript.py
@@ -10,6 +10,31 @@ from copy import deepcopy
 
 logger = logging.getLogger("pyState:Subscript")
 
+def _handleIndex(state,sub_object,sub_slice):
+
+    if type(sub_object) is not List:
+        err = "handleIndex: Don't know how to subscript type {0}".format(sub_object)
+        logger.error(err)
+        raise Exception(err)
+
+    # Resolve the index value
+    sub_index = state.resolveObject(sub_slice.value)
+
+    if sub_index.isStatic():
+        index = sub_index.getZ3Object().as_long()
+
+    # Check if it's a variable that only has one possibility
+    elif type(sub_index) in [Int, BitVec] and len(state.any_n_int(sub_index,2)) == 1:
+        index = state.any_int(sub_index)
+
+    else:
+        err = "handle: Don't know how to handle symbolic slice integers at the moment"
+        logger.error(err)
+        raise Exception(err)
+
+    return sub_object[index]
+
+
 def handle(state,element,ctx=None):
     """
     Input:
@@ -30,11 +55,6 @@ def handle(state,element,ctx=None):
     sub_slice = element.slice
     sub_value = element.value
 
-    if type(sub_slice) is not ast.Index:
-        err = "handle: Don't know how to handle slice type {0}".format(sub_slice)
-        logger.error(err)
-        raise Exception(err)
-
     if type(sub_value) not in [ast.Name,ast.Subscript]:
         err = "handle: Don't know how to handle value type {0}".format(sub_value)
         logger.error(err)
@@ -42,24 +62,11 @@ def handle(state,element,ctx=None):
 
     sub_object = state.resolveObject(sub_value,ctx=ctx)
 
-    if type(sub_object) is not List:
-        err = "handle: Don't know how to subscript type {0}".format(sub_object)
-        logger.error(err)
-        raise Exception(err)
-
-    # Resolve the index value
-    sub_index = state.resolveObject(sub_slice.value)
-
-    if sub_index.isStatic():
-        index = sub_index.getZ3Object().as_long()
-
-    # Check if it's a variable that only has one possibility
-    elif type(sub_index) in [Int, BitVec] and len(state.any_n_int(sub_index,2)) == 1:
-        index = state.any_int(sub_index)
+    if type(sub_slice) is ast.Index:
+        return _handleIndex(state,sub_object,sub_slice)
 
     else:
-        err = "handle: Don't know how to handle symbolic slice integers at the moment"
+        err = "handle: Don't know how to handle slice type {0}".format(sub_slice)
         logger.error(err)
         raise Exception(err)
 
-    return sub_object[index]
