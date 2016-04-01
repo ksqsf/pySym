f2465f62f068b47479c147b372385fda76b5abb3
Owlz <whootandahalf@gmail.com>
2016-03-31 20:19:43 -0400

    Added String.join method

---
+++ pyState/functions/String/join.py
@@ -0,0 +1,93 @@
+import logging
+from pyObjectManager.List import List
+from pyObjectManager.Int import Int
+from pyObjectManager.Real import Real
+from pyObjectManager.BitVec import BitVec
+from pyObjectManager.Char import Char
+from pyObjectManager.String import String
+import ast
+
+logger = logging.getLogger("pyState:SimFunction:String.join")
+
+
+def handle(state,call,elem):
+    """
+    Simulate Python's zfill string method
+    """
+    # The root (i.e.: "s" in s.join())
+    root = state.resolveObject(call.func.value)
+
+    assert type(root) is String
+
+    # Resolve the elem
+    elem = state.resolveObject(elem)
+    
+    if type(elem) is not List:
+        err = "handle: Don't know how to handle non-List join iterators"
+        logger.error(err)
+        raise Exception(err)
+
+
+    # Get new string
+    newString = state.getVar('tempStrJoin',ctx=1,varType=String)
+    newString.increment()
+
+    # Loop through elem values
+    for item in elem:
+        if type(item) is String:
+            newString.variables += item.variables + root.variables
+        else:
+            err = "handle: Don't know how to handle type {0}".format(type(item))
+            logger.error(err)
+            raise Exception(err)
+
+    if root.length() > 0:
+        # Remove the excess chars that may have built up
+        newString = newString[:-root.length()]
+
+    return newString.copy()
+
+    """
+    assert type(root) is String
+
+    # Resolve the width
+    width = state.resolveObject(width)
+
+    # TODO: Add symbolic width capability
+    if not width.isStatic():
+        err = "handle: Don't know how to handle non static width"
+        logger.error(err)
+        raise Exception(err)
+
+    # TODO: Add symbolic string capability
+    if not root.isStatic():
+        err = "handle: Don't know how to handle symbolic string"
+        logger.error(err)
+        raise Exception(err)
+
+    # Get new str
+    newString = state.getVar('tempZfillStr',ctx=1,varType=String)
+    newString.increment()
+    
+    # Resolve the width
+    width = width.getValue()
+
+    # zfill will not truncate
+    newString.setTo(root)
+    
+    # If we're already at our length, just return
+    if newString.length() >= width:
+        return newString
+
+    # Add as many "0"s as needed
+    while newString.length() < width:
+        # Create the new Char
+        c = state.getVar('tempCharZfill',ctx=1,varType=Char)
+        c.increment()
+        # Set it
+        c.setTo('0')
+        # Insert it
+        newString.variables.insert(0,c.copy())
+
+    return newString
+"""
---
+++ tests/test_function_String_join.py
@@ -0,0 +1,34 @@
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
+
+import logging
+import Colorer
+logging.basicConfig(level=logging.DEBUG,format='%(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+import ast
+import z3
+from pyPath import Path
+from pyPathGroup import PathGroup
+import pytest
+from pyObjectManager.Int import Int
+from pyObjectManager.Real import Real
+from pyObjectManager.BitVec import BitVec
+from pyObjectManager.List import List
+
+test1 = """
+x = ''.join(["1","2","3"])
+y = ','.join(["4","5","6"])
+"""
+
+
+def test_function_strJoin():
+    b = ast.parse(test1).body
+    p = Path(b,source=test1)
+    pg = PathGroup(p)
+
+    pg.explore()
+    assert len(pg.completed) == 1
+    
+    assert pg.completed[0].state.any_str('x') == ''.join(["1","2","3"])
+    assert pg.completed[0].state.any_str('y') == ','.join(["4","5","6"])
