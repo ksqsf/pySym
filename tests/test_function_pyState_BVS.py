ad0179c8430619f6a885d4adb2a990647cf89bcc
bannsec <whootandahalf@gmail.com>
2017-12-21 23:57:12 +0000

    pyState.BVS bugfix, var not incrementing

---
+++ tests/test_function_pyState_BVS.py
@@ -0,0 +1,36 @@
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+
+import logging
+from pySym import Colorer
+logging.basicConfig(level=logging.DEBUG,format='%(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+from pySym import ast_parse
+import z3
+from pySym.pyPath import Path
+from pySym.pyPathGroup import PathGroup
+import pytest
+from pySym.pyObjectManager.Int import Int
+from pySym.pyObjectManager.Real import Real
+from pySym.pyObjectManager.BitVec import BitVec
+from pySym.pyObjectManager.List import List
+from pySym.pyState.z3Helpers import Z3_MAX_STRING_LENGTH
+
+test1 = """
+s1 = pyState.BVS(1)
+s2 = pyState.BVS(1)
+"""
+
+def test_function_pyState_BVS_basic():
+    b = ast_parse.parse(test1).body
+    p = Path(b,source=test1)
+    pg = PathGroup(p)
+
+    pg.explore()
+    assert len(pg.completed) == 1
+    
+    s1 = pg.completed[0].state.getVar('s1')
+    s2 = pg.completed[0].state.getVar('s2')
+
+    # Make sure that we're not duplicating variables on creation
+    assert str(s1.getZ3Object()) != str(s2.getZ3Object())
